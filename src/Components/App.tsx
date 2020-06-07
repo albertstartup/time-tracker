9f277ac2f46e4dd925113d38b1ccc72cda295d49
Albert Martinez <albertstartup@gmail.com>
2020-06-07 13:02:27 -0700

    build: Add styled-components

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Form from "./Form";
 import List from "./List";
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 function App() {
   return (
-    <>
+    <AppWrapper>
       <Form/>
       <List/>
-    </>
+    </AppWrapper>
   );
 }
 
