82b1641881a28acfc4fe88e1b9bcb04411a0b2c8
Albert Martinez <albertstartup@gmail.com>
2020-09-25 12:42:28 -0700

    docs: Add jsdocs to minute rounding functions

--- src/lib.ts
+++ src/lib.ts
@@ -24,16 +24,50 @@ export const generateEntry = (
   };
 };
 
+/** Gets the last minute that is a multiple of 5 or 0.
+ * This function is used to generate startDate of a time entry.
+ * @example
+ * 4:O1 -> 4:00
+ * 4:17 -> 4:15
+ */
 export const getMinutesRoundedDown5 = (date: Date): number => {
   const minutes = date.getMinutes();
   return Math.floor(minutes / 5) * 5;
 };
 
+/** Gets the next minute that is a multiple of 5.
+ * @example
+ * 4:08 -> 4:10
+ */
 export const getMinutesRoundedUp5 = (date: Date): number => {
   const minutes = date.getMinutes();
   return Math.ceil(minutes / 5) * 5;
 };
 
+/** Gets how many more minutes until the next multiple of 5.
+ * @example
+ * 4:03 -> 2
+ */
+export const getMinutesFromNext5 = (date: Date) => {  
+  return getMinutesRoundedUp5(date) - date.getMinutes()
+}
+
+/** Gets how many milliseconds are left until the next minute of multiple 5.
+ * @example
+ * 4:04 -> 60,000
+ * @example
+ * 4:00 -> 300,000
+ */
+export const getMillisecondsFromNext5 = (date: Date) => {
+  const minutesInMilliseconds = getMinutesFromNext5(date) * 60000
+  const secondsInMilliseconds = (60 - date.getSeconds()) * 1000
+  return minutesInMilliseconds + secondsInMilliseconds
+}
+
+/** Pads a single digit number with a leading zero to make it two digits.
+ * @example
+ * 1 -> 01
+*/
 const padNumber = (num: number) => num.toString().padStart(2, "0");
 
 export const parseJSONDate = (str: string) => new Date(JSON.parse(str));
@@ -46,7 +80,7 @@ export const displayStartDate = (startDate: string) => {
   return displayHours + ":" + padNumber(minutes);
 };
 
-/** Convert startDate to a string that can be passed as a value to an input element */
+/** Convert startDate to a string that can be passed as a value to an input element. */
 export const startDateToInputValue = (startDate: string) =>
   padNumber(parseJSONDate(startDate).getHours()) +
   ":" +
